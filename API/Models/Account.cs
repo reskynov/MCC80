fa5c0b00591b6e687a6f1e6a6c84d35d8bdc0a51
reskynov <reskykiky2000@gmail.com>
2023-07-18 09:17:28 +0700

    Update Account.cs

--- API/Models/Account.cs
+++ API/Models/Account.cs
@@ -3,7 +3,7 @@
     public class Account
     {
         public Guid Guid { get; set; }
-        public int Otp { get; set; }
+        public int OTP { get; set; }
         public bool IsUsed { get; set; }
         public DateTime ExpiredDate { get; set; }
         public DateTime CreatedDate { get; set; }
