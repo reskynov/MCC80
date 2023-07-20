fba12358c692bff62af664da030e9f4cbeefb532
reskynov <reskykiky2000@gmail.com>
2023-07-20 14:54:43 +0700

    add API Crud account

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/accounts")]
+    public class AccountController : Controller
+    {
+        
+    }
+}
---
+++ API/Repositories/AccountRepository.cs
@@ -0,0 +1,13 @@
+﻿using API.Contracts;
+using API.Data;
+using API.Models;
+
+namespace API.Repositories
+{
+    public class AccountRepository : GenericRepository<Account>, IAccountRepository
+    {
+        public AccountRepository(BookingDbContext context) : base(context)
+        {
+        }
+    }
+}
